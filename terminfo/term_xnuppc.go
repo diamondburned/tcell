39c5bdba8a047dbca76e9a7af22b8bc5f2b66b8e
diamondburned (Forefront) <datutbrus@gmail.com>
2019-03-09 17:09:39 -0800

    fixed term cherry merge errors

--- terminfo/term_xnuppc.go
+++ terminfo/term_xnuppc.go
@@ -5,30 +5,6 @@ package terminfo
 func init() {
 	// Darwin PowerPC Console (color)
 	AddTerminfo(&Terminfo{
-<<<<<<< HEAD
-		Name:         "xnuppc",
-		Aliases:      []string{"darwin"},
-		Colors:       8,
-		Clear:        "\x1b[H\x1b[J",
-		AttrOff:      "\x1b[m",
-		Underline:    "\x1b[4m",
-		Bold:         "\x1b[1m",
-		Reverse:      "\x1b[7m",
-		EnterKeypad:  "\x1b[?1h\x1b=",
-		ExitKeypad:   "\x1b[?1l\x1b>",
-		SetFg:        "\x1b[3%p1%dm",
-		SetBg:        "\x1b[4%p1%dm",
-		SetFgBg:      "\x1b[3%p1%d;4%p2%dm",
-		PadChar:      "\x00",
-		SetCursor:    "\x1b[%i%p1%d;%p2%dH",
-		CursorBack1:  "\x1b[D",
-		CursorUp1:    "\x1b[A",
-		KeyUp:        "\x1bOA",
-		KeyDown:      "\x1bOB",
-		KeyRight:     "\x1bOC",
-		KeyLeft:      "\x1bOD",
-		KeyBackspace: "\u007f",
-=======
 		Name:          "xnuppc",
 		Aliases:       []string{"darwin"},
 		Colors:        8,
@@ -53,6 +29,5 @@ func init() {
 		KeyRight:      "\x1bOC",
 		KeyLeft:       "\x1bOD",
 		KeyBackspace:  "\xff",
->>>>>>> 703b3f6... Hack in strikethrough and italic
 	})
 }
